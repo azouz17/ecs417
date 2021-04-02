c63cf3b3ce30446432e0f60854a23ab6423165f2
azouz17 <78298485+azouz17@users.noreply.github.com>
2021-04-02 12:39:00 +0000

    changed

--- webroot/mini-project/add_entry.php
+++ webroot/mini-project/add_entry.php
@@ -20,8 +20,8 @@ $conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
 if ($conn->connect_error) {
  die("Connection failed: " . $conn->connect_error);
 }
-$sql=SELECT "username" FROM LOGIN;
-$sqls= SELECET "password" FROM LOGIN;
+$sql="SELECT username FROM LOGIN";
+$sqls= "SELECT password FROM LOGIN";
 
 if($_GET["username"]!=$sql | $_GET["password"]!=$sqls)
 {
