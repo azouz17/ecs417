022516f078c90ae14617607f436e09b650a7ea0f
azouz17 <78298485+azouz17@users.noreply.github.com>
2021-04-08 13:09:14 +0000

    changed

--- webroot/mini-project/redirect.php
+++ webroot/mini-project/redirect.php
@@ -4,6 +4,39 @@
   <title> </title>
 </head>
 <?php
+//Expire the session if user is inactive for 30
+//minutes or more.
+$expireAfter = 30;
+
+//Check to see if our "last action" session
+//variable has been set.
+if(isset($_SESSION['last_action'])){
+
+    //Figure out how many seconds have passed
+    //since the user was last active.
+    $secondsInactive = time() - $_SESSION['last_action'];
+
+    //Convert our minutes into seconds.
+    $expireAfterSeconds = $expireAfter * 60;
+
+    //Check to see if they have been inactive for too long.
+    if($secondsInactive >= $expireAfterSeconds){
+        //User has been inactive for too long.
+        //Kill their session.
+        session_unset();
+        session_destroy();
+        header("Location:portfolio.html");
+    }
+
+}
+
+//Assign the current timestamp as the user's
+//latest activity
+$_SESSION['last_action'] = time();
+
+
+ ?>
+<?php
 $user =$_GET["username"];
 $pass =$_GET["password"];
 $dbhost = getenv("MYSQL_SERVICE_HOST");
@@ -29,6 +62,7 @@ if ($result->num_rows > 0) {
 if($row["username"]===$user and $row["password"]===$pass){
   session_start();
   $_SESSION['state']="yes";
+  $_SESSION['last_action']=time();
 header("Location:add_entry.html");
 exit();
 }
