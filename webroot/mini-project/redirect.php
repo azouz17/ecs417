c675804ff9e21c2cc5f9b416f5496641c6851419
azouz17 <78298485+azouz17@users.noreply.github.com>
2021-04-10 12:11:48 +0000

    changed

--- webroot/mini-project/redirect.php
+++ webroot/mini-project/redirect.php
@@ -52,7 +52,7 @@ $conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
 if ($conn->connect_error) {
  die("Connection failed: " . $conn->connect_error);
 }
-$sql="SELECT username,password FROM LOGIN";
+$sql="SELECT username,password FROM login";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
