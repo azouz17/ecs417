8148bfd0531fde583334516c69fb8d2ff55562c7
azouz17 <78298485+azouz17@users.noreply.github.com>
2021-04-11 15:10:27 +0000

    changed

--- webroot/test.php
+++ webroot/test.php
@@ -16,6 +16,8 @@ die("Connection failed: " . $conn->connect_error);
 $sql="SELECT admin FROM login";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
+
+  $row = $result->fetch_assoc();
   echo "Inside if statment";
     // output data of each row
     echo $row['admin'];
